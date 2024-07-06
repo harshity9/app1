2e01f5dbe63dac540574e029b4eff153d1aab6ad
harshit <harshityadav0927@gmail.com>
2024-07-06 15:46:10 +0530

    final-v

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [newValue, setInputValue]= useState('');
+  const handleChange=(event)=>{
+  const newValue= event.target.value 
+  setInputValue(newValue);
+  console.log(newValue)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+          <section className="main" style={{backgroundColor: newValue}}></section>
+          <input type="text" placeholder="Enter your color" onChange={handleChange} className="nameBox"></input>
+      
+      </div>
+      );
 }
 
 export default App;
